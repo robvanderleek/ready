c5a74978ed32c0430c476dee4bd931687cd55f95
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-05-11 22:10:55 +0200

    Class to function component

--- src/context/DeviceContext.jsx
+++ src/context/DeviceContext.jsx
@@ -1,41 +1,26 @@
-import React, {Component, useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 const DeviceContext = React.createContext({});
 
-export class DeviceProvider extends Component {
+export function DeviceProvider(props) {
+    const [initializing, setInitializing] = useState(true);
+    const [isTouchDevice, setIsTouchDevice] = useState(false);
+    const [forceNonTouchDevice, setForceNonTouchDevice] = useState(false);
+    const [orientation, setOrientation] = useState(null);
+    const [screenWidth, setScreenWidth] = useState(window.screen.width);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            initializing: true,
-            isTouchDevice: false,
-            forceNonTouchDevice: false,
-            orientation: null,
-            screenWidth: window.screen.width
-        }
-    }
-
-    componentDidMount() {
-        this.addOrientationChangeListener();
-        setInterval(this.checkOrientation, 2000);
-        this.checkDevice();
-        this.checkOrientation();
-        this.setState({initializing: false});
-    }
-
-    addOrientationChangeListener = () => {
+    const addOrientationChangeListener = () => {
         const supportsOrientationChange = "onorientationchange" in window;
         const orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
         window.addEventListener(orientationEvent, () => this.checkOrientation(), false);
     }
 
-    checkDevice = () => {
+    const checkDevice = () => {
         const touchDeviceCheckResult = ('ontouchstart' in document.documentElement);
-        this.setState({isTouchDevice: touchDeviceCheckResult});
+        setIsTouchDevice(touchDeviceCheckResult);
     }
 
-    checkOrientation = () => {
-        const {screenWidth} = this.state;
+    const checkOrientation = () => {
         const newScreenWidth = window.screen.width;
         const newScreenHeight = window.screen.height;
         let orientation;
@@ -48,19 +33,25 @@ export class DeviceProvider extends Component {
             const mql = window.matchMedia("(orientation: portrait)");
             orientation = mql.matches ? 'portrait' : 'landscape';
         }
-        this.setState({orientation: orientation, screenWidth: newScreenWidth});
+        setOrientation(orientation);
+        setScreenWidth(newScreenWidth);
     }
 
-    render() {
-        const {children} = this.props;
-        return (
-            <DeviceContext.Provider value={{
-                ...this.state
-            }}>
-                {children}
-            </DeviceContext.Provider>
-        );
-    }
+    useEffect(() => {
+        addOrientationChangeListener();
+        setInterval(checkOrientation, 2000);
+        checkDevice();
+        checkOrientation();
+        setInitializing(false);
+    }, []);
+
+    return (
+        <DeviceContext.Provider value={{
+            initializing, isTouchDevice, forceNonTouchDevice, setForceNonTouchDevice, orientation
+        }}>
+            {props.children}
+        </DeviceContext.Provider>
+    );
 
 }
 
